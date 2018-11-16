3cfd3813abd678443462fb115d7ba121dbb3bac5
Lindsay Landry <lindsay@animoto.com>
2018-11-16 15:25:38 -0500

    add main.go back in, remove erroneous file

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "flag"
+
+  "github.com/lindsaylandry/go-cross-stitch/src/convert"
+)
+
+//go:generate go run scripts/setupColors.go
+
+func main() {
+  if len(os.Args) < 2 {
+    fmt.Println("No input image provided")
+    os.Exit(0)
+  }
+ 
+  num := flag.Int("n", 6, "number of colors to use (2^n)")
+  flag.Parse()
+
+  err := convert.DMC(flag.Args()[0], *num)
+  if err != nil { panic(err) }
+}
